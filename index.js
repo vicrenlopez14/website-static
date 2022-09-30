296d78a2aea86cb93f4f0e8203b19c5c041492ba
Víctor López <vicren.lopez@outlook.com>
2022-09-30 09:27:16 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+const path = require('path')
+
+app.use(express.static(path.join(__dirname, 'public/Español')));
+app.use(express.static(path.join(__dirname, 'public/Inglés')));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, "./public/Inglés/index.html"));
+});
+
+app.get('/es', (req, res) => {
+    res.sendFile(path.join(__dirname, "./public/Español/Español.html"));
+});
+
+app.get('/en', (req, res) => {
+    res.sendFile(path.join(__dirname, "./public/Inglés/index.html"));
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
